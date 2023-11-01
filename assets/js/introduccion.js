e046b921b7013d8926e556ec3561af9bc413c658
anamarigarzon <anamari.garzon@urosario.edu.co>
2023-11-01 12:48:22 -0500

    Add JavaScript Intro

---
+++ assets/js/introduccion.js
@@ -0,0 +1,37 @@
+/*scripts ECMaScripts  isomórfico/
+//comentarios
+//variables
+
+/*
+$name
+_name
+name
+
+nombrePersona
+
+let numero = 1;
+let nombre = 'Ana';
+let miVariable = true;
+let miVariable1 = [1,''bob];
+let suma = num1+num2;
+
+*/
+
+
+
+/* operadores 
+aritméticos + - * / mod %
+de comparación == === != !== < > <= >=
+lógicos && || !
+*/
+
+/*
+console.info()
+console.error()
+console.warn()
+console.clear()
+*/
+
+let num1 = parseInt(prompt('digite el primer número'))
+let num2 = parseInt(prompt('digite el segundo número'))
+console.log(`la suma es ${num1+num2}`)
